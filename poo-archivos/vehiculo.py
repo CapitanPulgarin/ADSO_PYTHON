975b7bd2e77d834c3c42348f6b9a28e5240f39c4
CapitanPulgarin <116228654+CapitanPulgarin@users.noreply.github.com>
2023-03-10 17:25:30 -0500

    Create vehiculo.py

---
+++ poo-archivos/vehiculo.py
@@ -0,0 +1,4 @@
+class Vehiculo: # se crea una clase 
+    def __init__(self,placa,conductor): # se inicializa un constructor con parametros asignados
+        self.__placa=placa # se le asigna el valor del atributo
+        self.__conductor=conductor # se le asigna el valor del atributo
