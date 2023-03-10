f138d902023514a8b546c8c922221ce79ffdf98c
CapitanPulgarin <116228654+CapitanPulgarin@users.noreply.github.com>
2023-03-10 17:12:41 -0500

    Update match.py.py

--- match.py.py
+++ match.py.py
@@ -56,6 +56,63 @@ class estudiante:
     def admit(self):
         return self.Esco_curso
     
+class register:
+    def __init__ (self,id,nombre,edad,num_conta,nom_usu,contraseña):
+        self.id=id
+        self.nombre=nombre
+        self.edad=edad
+        self.num_conta=num_conta
+        self.nom_usu=nom_usu
+        self.contraseña=contraseña
+
+    
+    def agrid(self):
+        return self.id
+    def agrnombre(self):
+        return self.nombre
+    def agredad(self):
+        return self.edad
+    def agrnum_tel(self):
+        return self.num_conta   
+    def agrnom_usu(self):
+        return self.nom_usu
+    def agrclave(self):
+        return self.contraseña
+    
+class instructor(register):
+    def __init__(self,Usu_nom,Cla_instructor):
+        self.Usu_nom=Usu_nom
+        self.Cla_istructor=Cla_instructor
+
+    def getN_inst(self):
+        return self.Usu_nom
+    
+    def getC_inst(self):
+        return self.Cla_istructor
+
+class transaciones:
+    def __init__ (self,T_ident,T_Detalles,T_fecha,mediopago):
+        self.T_ident=T_ident
+        self.T_Detalles=T_Detalles
+        self.T_fecha=T_fecha
+        self.mediopago=mediopago
+    def agregaID(self):
+        return self.T_ident
+    def agregaDET(self):
+        return self.T_Detalles
+    def agregafecha(self):
+        return self.T_fecha
+    def agregamedipago(self):
+        return self.mediopago
+    
+class estudiante:
+    def __init__ (self,Esco_curso,Actualizar):
+        self.Esco_curso=Esco_curso
+        self.Actualizar=Actualizar
+
+    def admit(self):
+        return self.Esco_curso
+    
     def update(self):
         return self.Actualizar
     
@@ -122,7 +179,7 @@ while True:
             l=str(input('ingrese su usuario: '))  
             j=int(input('ingrese su Contraseña: '))
             if l  == it.getN_inst():
-                if j == it.getC_inst():
+                if j == int(it.getC_inst()): # casting convertir str en int
                     print('BIENVENIDO INSTRUCTOR',it.getN_inst())
                 else:
                     print('error vuelva a ingresar sus credenciales XD')    
@@ -141,7 +198,7 @@ while True:
                         it.agrnom_usu=d
                         e=input('ingresed una Contraseña: ')
                         it.agrclave=e
-                        print('ID: ',it.agrid() +'\n Nombre y Apellido: '+ it.agrnombre() +'\n Edad: '+ it.agredad() +'\n Numero telefonico: '+ it.agrnum_tel() +'\n NOmbre Usuario: '+it.agrnom_usu() + '\n Clave: '+ it.agrclave())
+                        print('ID: '+ it.agrid() +'\n Nombre y Apellido: '+ it.agrnombre() +'\n Edad: '+ it.agredad() +'\n Numero telefonico: '+ it.agrnum_tel() +'\n NOmbre Usuario: '+it.agrnom_usu() + '\n Clave: '+ it.agrclave())
                 elif ll == '2':
                         print('Comprobante de cupo terminada'+'\n ID: ' +tr.agregaID() + '\n Detalles: '+tr.agregaDET()+'\n Fecha: '+ tr.agregafecha() + '\n Manera de pago: '+ tr.agregamedipago())
         case '2':
@@ -158,8 +215,7 @@ while True:
             e=input('ingresed una Contraseña: ')
             e=re.agrclave
             print('\n ID: '+re.agrid() +'\n Nombre y Apellido: '+ re.agrnombre() +'\n Edad: '+ re.agredad() +'\n Numero telefonico: '+ re.agrnum_tel() +'\n NOmbre Usuario: '+re.agrnom_usu() + '\n Clave: '+ re.agrclave())
-            
-
+            break
 
 
 
